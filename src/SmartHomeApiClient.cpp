d93b43ed6983e0eebfcd43858cf541fd49b9fd11
Piotr Szulc <szulo123@gmail.com>
2022-03-29 23:20:21 +0200

    Filling relay data in vector

--- src/SmartHomeApiClient.cpp
+++ src/SmartHomeApiClient.cpp
@@ -15,14 +15,12 @@ bool SmartHomeApiClient::GetRelays(std::vector<Relay> *&relays)
     _http.begin(_baseUrl + "relay");
 
     auto code = _http.GET();
-    if (!code) {
+    if (!code)
+    {
         _http.end();
         Serial.println("Error during http request");
         return false;
     }
-
-    Serial.print("HTTP code=");
-    Serial.println(code);
 
     StaticJsonDocument<1024> doc;
 
@@ -33,26 +31,23 @@ bool SmartHomeApiClient::GetRelays(std::vector<Relay> *&relays)
     // auto error = deserializeJson(doc, response);
     _http.end();
 
-    if (error) {
+    if (error)
+    {
         Serial.print(F("deserializeJson() failed with code "));
         Serial.println(error.f_str());
         return false;
     }
 
+    relays = new std::vector<Relay>(doc.size());
 
-    Serial.printf("Elements in response: %d\n", doc.size());
-    relays = new std::vector<Relay>();
-
+    int i = 0;
     for (auto rel : doc.as<JsonArray>())
     {
         auto obj = rel.as<JsonObject>();
-
-        Relay *r = new Relay();
-        r->Id = obj["id"].as<GUID>();
-        r->Type = obj["type"].as<String>();
-        r->Name = obj["name"].as<String>();
-
-        relays->push_back(*r);
+        (*relays)[i].Id = obj["id"].as<GUID>();
+        (*relays)[i].Type = obj["type"].as<String>();
+        (*relays)[i].Name = obj["name"].as<String>();
+        i++;
     }
 
     return true;
